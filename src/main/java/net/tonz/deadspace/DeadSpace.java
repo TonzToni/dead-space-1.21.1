be7dc26c7266918cb98b08feea925830eb28666a
TonzToni <98320002+TonzToni@users.noreply.github.com>
2025-06-29 23:40:07 +0100

    Initial commit

---
+++ src/main/java/net/tonz/deadspace/DeadSpace.java
@@ -0,0 +1,16 @@
+package net.tonz.deadspace;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DeadSpace implements ModInitializer {
+	public static final String MOD_ID = "deadspace";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/tonz/deadspace/DeadSpaceClient.java
@@ -0,0 +1,10 @@
+package net.tonz.deadspace;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DeadSpaceClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
