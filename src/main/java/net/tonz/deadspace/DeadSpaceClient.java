8df80711fbe1dd0042c3da702cf831d8f73ef746
TonzToni <98320002+TonzToni@users.noreply.github.com>
2025-07-02 00:16:58 +0100

    got ModCamera to spawn ontop of the display block and output its position into the terminal

--- src/main/java/net/tonz/deadspace/DeadSpaceClient.java
+++ src/main/java/net/tonz/deadspace/DeadSpaceClient.java
@@ -10,19 +10,6 @@ import net.tonz.deadspace.camera.CameraStorage;
 public class DeadSpaceClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        // Print position every 20 ticks (~1 second)
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (client.world == null) return;
 
-            tickCounter++;
-            if (tickCounter >= 20) {
-                tickCounter = 0;
-
-                if (CameraStorage.camera != null) {
-                    System.out.println("Camera at: " + CameraStorage.camera.getPos());
-                }
-            }
-        });
     }
-    private int tickCounter = 0;
 }
